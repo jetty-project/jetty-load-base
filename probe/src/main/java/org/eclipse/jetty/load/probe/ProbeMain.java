ac65799461b62056b33951201d7db34933a03251
olivier lamy <olamy@apache.org>
2017-09-23 21:51:51 +1000

    add some logs
    Signed-off-by: olivier lamy <olamy@apache.org>

--- probe/src/main/java/org/eclipse/jetty/load/probe/ProbeMain.java
+++ probe/src/main/java/org/eclipse/jetty/load/probe/ProbeMain.java
@@ -6,11 +6,14 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jetty.load.LiveLatencyDisplayListener;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import org.mortbay.jetty.load.generator.LoadGenerator;
 import org.mortbay.jetty.load.generator.starter.LoadGeneratorStarter;
 import org.mortbay.jetty.load.generator.starter.LoadGeneratorStarterArgs;
 
 public class ProbeMain {
+    private static final Logger LOGGER = Log.getLogger( ProbeMain.class);
     public static void main(String[] args) {
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
@@ -20,9 +23,14 @@ public class ProbeMain {
         builder = builder.resourceListener(listener).listener(listener);
 
         ScheduledFuture<?> task = scheduler.scheduleWithFixedDelay(listener, 1, 1, TimeUnit.SECONDS);
+        LOGGER.info( "start load generator run" );
+        long start = System.currentTimeMillis();
         try {
             LoadGeneratorStarter.run(builder);
         } finally {
+            long end = System.currentTimeMillis();
+            LOGGER.info( "end load generator run {} seconds", //
+                         TimeUnit.SECONDS.convert( end - start, TimeUnit.MILLISECONDS ) );
             task.cancel(false);
             scheduler.shutdown();
         }
