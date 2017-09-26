04e67a3724ba67a1ff3f83a9eb3766fe54c0d159
olivier lamy <olamy@apache.org>
2017-09-26 15:05:33 +1000

    add servlet to get some runtime information when recording result
    Signed-off-by: olivier lamy <olamy@apache.org>

---
+++ test-webapp/src/main/java/com/acme/ServerInfoServlet.java
@@ -0,0 +1,69 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2017 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package com.acme;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServerInfoServlet
+    extends HttpServlet
+{
+
+    @Override
+    protected void doGet( HttpServletRequest req, HttpServletResponse resp )
+        throws ServletException, IOException
+    {
+        new ObjectMapper().writeValue( resp.getOutputStream(), new ServerInfo() );
+    }
+
+
+    public static class ServerInfo
+    {
+
+        private String jettyVersion = System.getProperty( "jetty.version" );
+        //System.setProperty("jetty.tag.version",tag)
+
+        private int availableProcessors = Runtime.getRuntime().availableProcessors();
+
+        private long totalMemory = Runtime.getRuntime().totalMemory();
+
+        public String getJettyVersion()
+        {
+            return jettyVersion;
+        }
+
+        public int getAvailableProcessors()
+        {
+            return availableProcessors;
+        }
+
+        public long getTotalMemory()
+        {
+            return totalMemory;
+        }
+
+    }
+
+
+}
