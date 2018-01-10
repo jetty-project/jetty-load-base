8bb0b245b09fd3ba60b829a23a8a9d4429434ff6
Simone Bordet <simone.bordet@gmail.com>
2018-01-10 23:28:11 +0100

    Reworked the probe.
    Moved LiveLatencyDisplayListener to the probe module.

---
+++ probe/src/main/java/org/eclipse/jetty/load/probe/LiveLatencyDisplayListener.java
@@ -0,0 +1,99 @@
+package org.eclipse.jetty.load.probe;
+
+import java.util.concurrent.TimeUnit;
+
+import org.HdrHistogram.Histogram;
+import org.HdrHistogram.Recorder;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.load.Monitor;
+import org.eclipse.jetty.toolchain.perf.HistogramSnapshot;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+import org.mortbay.jetty.load.generator.LoadGenerator;
+import org.mortbay.jetty.load.generator.Resource;
+import org.mortbay.jetty.load.generator.listeners.CollectorInformations;
+import org.mortbay.jetty.load.generator.listeners.LoadConfig;
+import org.mortbay.jetty.load.generator.listeners.LoadResult;
+import org.mortbay.jetty.load.generator.listeners.ServerInfo;
+
+public class LiveLatencyDisplayListener extends Request.Listener.Adapter implements Resource.NodeListener, LoadGenerator.BeginListener, LoadGenerator.EndListener, Runnable {
+    private static final Logger LOGGER = Log.getLogger(LiveLatencyDisplayListener.class);
+
+    private final Recorder recorder;
+    private final Histogram histogram;
+    private Histogram interval;
+    private Monitor.Start begin;
+    private Monitor.Start start;
+    private ServerInfo serverInfo;
+    private LoadResult loadResult;
+    private LoadConfig.Type loadConfigType;
+
+    public LiveLatencyDisplayListener() {
+        this(TimeUnit.MICROSECONDS.toNanos(1), TimeUnit.SECONDS.toNanos(60), 3);
+    }
+
+    public LiveLatencyDisplayListener(long low, long high, int digits) {
+        histogram = new Histogram(low, high, digits);
+        recorder = new Recorder(low, high, digits);
+        start = Monitor.start();
+    }
+
+    public LiveLatencyDisplayListener serverInfo(ServerInfo serverInfo) {
+        this.serverInfo = serverInfo;
+        return this;
+    }
+
+    public LiveLatencyDisplayListener loadConfigType(LoadConfig.Type loadConfigType) {
+        this.loadConfigType = loadConfigType;
+        return this;
+    }
+
+    public void onResourceNode(Resource.Info info) {
+        long responseTime = info.getResponseTime() - info.getRequestTime();
+        recorder.recordValue(responseTime);
+    }
+
+    public void run() {
+        Monitor.Stop stop = start.stop();
+
+        interval = recorder.getIntervalHistogram(interval);
+        histogram.add(interval);
+
+        long elapsed = interval.getEndTimeStamp() - interval.getStartTimeStamp();
+        long responseRate = elapsed > 0 ? interval.getTotalCount() * 1000 / elapsed : -1;
+
+        LOGGER.info(String.format("response rate=%s, cpu=%.2f%%, jit=%d ms, time min/mdn/max=%d/%d/%d \u00B5s",
+                responseRate,
+                stop.cpuPercent,
+                stop.deltaJITTime,
+                TimeUnit.NANOSECONDS.toMicros(interval.getMinValue()),
+                TimeUnit.NANOSECONDS.toMicros(interval.getValueAtPercentile(50)),
+                TimeUnit.NANOSECONDS.toMicros(interval.getMaxValue())));
+
+        this.start = Monitor.start();
+    }
+
+    @Override
+    public void onBegin(LoadGenerator loadGenerator) {
+        begin = Monitor.start();
+    }
+
+    @Override
+    public void onEnd(LoadGenerator loadGenerator) {
+        Monitor.Stop end = begin.stop();
+        long elapsed = TimeUnit.NANOSECONDS.toMillis(histogram.getEndTimeStamp() - histogram.getStartTimeStamp());
+        long rate = elapsed > 0 ? histogram.getTotalCount() * 1000 / elapsed : -1;
+        LOGGER.info(String.format("rate=%d, cpu=%.2f%%, jit=%d ms", rate, end.cpuPercent, end.deltaJITTime));
+        HistogramSnapshot snapshot = new HistogramSnapshot(histogram, 20, "response time",
+                "\u00B5s", TimeUnit.NANOSECONDS::toMicros);
+        System.err.println(snapshot);
+
+        CollectorInformations collectorInformations = new CollectorInformations(histogram);
+        loadResult = new LoadResult(serverInfo, collectorInformations,
+                new LoadConfig(loadGenerator.getConfig()).type(loadConfigType));
+    }
+
+    public LoadResult getLoadResult() {
+        return loadResult;
+    }
+}
--- probe/src/main/java/org/eclipse/jetty/load/probe/ProbeMain.java
+++ probe/src/main/java/org/eclipse/jetty/load/probe/ProbeMain.java
@@ -1,14 +1,24 @@
 package org.eclipse.jetty.load.probe;
 
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 import com.beust.jcommander.DynamicParameter;
-import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jetty.load.LiveLatencyDisplayListener;
 import org.eclipse.jetty.load.Version;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
+import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
+import org.eclipse.jetty.util.thread.Scheduler;
 import org.mortbay.jetty.load.generator.LoadGenerator;
 import org.mortbay.jetty.load.generator.listeners.LoadConfig;
 import org.mortbay.jetty.load.generator.listeners.LoadResult;
@@ -17,137 +27,80 @@ import org.mortbay.jetty.load.generator.starter.LoadGeneratorStarter;
 import org.mortbay.jetty.load.generator.starter.LoadGeneratorStarterArgs;
 import org.mortbay.jetty.load.generator.store.ResultStore;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+public class ProbeMain {
+    private static final Logger LOGGER = Log.getLogger(ProbeMain.class);
 
-public class ProbeMain
-{
-    private static final Logger LOGGER = Log.getLogger( ProbeMain.class );
+    public static void main(String[] args) throws Exception {
+        ProbeArgs probeArgs = LoadGeneratorStarter.parse(args, ProbeArgs::new);
+        LoadGenerator.Builder builder = LoadGeneratorStarter.prepare(probeArgs);
 
-    public static void main( String[] args )
-        throws Exception
-    {
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        Scheduler scheduler = new ScheduledExecutorScheduler();
+        scheduler.start();
+        builder.scheduler(scheduler);
 
-        ProbeArgs starterArgs = parse( args );
-
-        ServerInfo serverInfo = ServerInfo.retrieveServerInfo( starterArgs.getScheme(), //
-                                                               starterArgs.getHost(), //
-                                                               starterArgs.getPort(), //
-                                                               "/test/info/" );
-
-        LOGGER.info( "run load test on server:{}", serverInfo );
-        LOGGER.info( "Probe version: {}", Version.getInstance() );
-
-        LoadGenerator.Builder builder = LoadGeneratorStarter.prepare( starterArgs );
-        LiveLatencyDisplayListener listener = new LiveLatencyDisplayListener().serverInfo( serverInfo )
-            .loadConfigType( LoadConfig.Type.PROBE );
-        builder = builder.resourceListener( listener ).listener( listener );
-
-        ScheduledFuture<?> task = scheduler.scheduleWithFixedDelay( listener, 3, 3, TimeUnit.SECONDS );
-        LOGGER.info( "start load generator run" );
-        long start = System.currentTimeMillis();
         try {
-            LoadGeneratorStarter.run( builder );
+            ServerInfo serverInfo = ServerInfo.retrieveServerInfo(probeArgs.getScheme(),
+                    probeArgs.getHost(),
+                    probeArgs.getPort(),
+                    "/test/info/");
+
+            LOGGER.info("run load test on server:{}", serverInfo);
+            LOGGER.info("Probe version: {}", Version.getInstance());
+
+            LiveLatencyDisplayListener listener = new LiveLatencyDisplayListener().serverInfo(serverInfo)
+                    .loadConfigType(LoadConfig.Type.PROBE);
+            builder = builder.listener(listener).resourceListener(listener).requestListener(listener);
+
+            // Print probe activity periodically.
+            schedule(scheduler, new Runnable() {
+                @Override
+                public void run() {
+                    listener.run();
+                    schedule(scheduler, this);
+                }
+            });
+
+            LOGGER.info("start load generator run");
+            long start = System.nanoTime();
+            LoadGeneratorStarter.run(builder);
+            long end = System.nanoTime();
+            LOGGER.info("end load generator run {} seconds", TimeUnit.NANOSECONDS.toSeconds(end - start));
+
+            LoadResult loadResult = listener.getLoadResult();
+            String comment = probeArgs.dynamicParams.get("loadresult.comment");
+            if (StringUtils.isNotEmpty(comment)) {
+                loadResult.setComment(comment);
+            }
+
+            StringWriter stringWriter = new StringWriter();
+            new ObjectMapper().writeValue(stringWriter, loadResult);
+            String jsonLoadResult = stringWriter.toString();
+            LOGGER.info("loadResult json: {}", jsonLoadResult);
+
+            String resultFilePath = probeArgs.resultFilePath;
+            if (resultFilePath != null) {
+                Path path = Paths.get(resultFilePath);
+                Files.deleteIfExists(path);
+                Files.write(path, jsonLoadResult.getBytes(StandardCharsets.UTF_8));
+            }
+
+            List<ResultStore> resultStores = ResultStore.getActives(probeArgs.dynamicParams);
+            resultStores.forEach(resultStore -> resultStore.initialize(probeArgs.dynamicParams));
+            resultStores.forEach(resultStore -> resultStore.save(loadResult));
         } finally {
-            long end = System.currentTimeMillis();
-            LOGGER.info( "end load generator run {} seconds", //
-                         TimeUnit.SECONDS.convert( end - start, TimeUnit.MILLISECONDS ) );
-            task.cancel( false );
-            scheduler.shutdown();
+            scheduler.stop();
         }
-
-        // we can record result
-        // this can be moved to a resultStore implementation
-        LoadResult loadResult = listener.getLoadResult();
-        String comment = starterArgs.getDynamicParams().get( "loadresult.comment" );
-        if ( StringUtils.isNotEmpty( comment ) )
-        {
-            loadResult.setComment( comment );
-        }
-
-        StringWriter stringWriter = new StringWriter();
-        try
-        {
-            new ObjectMapper().writeValue( stringWriter, loadResult );
-        }
-        catch ( IOException e )
-        {
-            LOGGER.info( "skip cannot write LoadResult as json", e );
-        }
-
-        LOGGER.info( "loadResult json: {}", stringWriter.toString() );
-
-        if ( starterArgs.getResultFilePath() != null )
-        {
-            Path path = Paths.get( starterArgs.getResultFilePath() );
-            Files.deleteIfExists( path );
-            Files.write( path, stringWriter.toString().getBytes() );
-        }
-
-        List<ResultStore> resultStores = ResultStore.getActives( starterArgs.getDynamicParams() );
-
-        resultStores.stream() //
-            .forEach( resultStore -> resultStore.initialize( starterArgs.getDynamicParams() ) );
-
-        resultStores.stream() //
-            .forEach( resultStore -> resultStore.save( loadResult ) );
-
-        System.exit( 0 );
     }
 
+    private static void schedule(Scheduler scheduler, Runnable task) {
+        scheduler.schedule(task, 2, TimeUnit.SECONDS);
+    }
 
-    public static class ProbeArgs
-        extends LoadGeneratorStarterArgs
-    {
-        @Parameter( names = { "--result-path", "-rp" }, description = "Path to store json result file" )
+    private static class ProbeArgs extends LoadGeneratorStarterArgs {
+        @Parameter(names = {"--result-path", "-rp"}, description = "Path to store json result file")
         private String resultFilePath;
 
-        @DynamicParameter( names = "-D", description = "Dynamic parameters go here" )
-        public Map<String, String> dynamicParams = new HashMap<>();
-
-        public String getResultFilePath()
-        {
-            return resultFilePath;
-        }
-
-        public void setResultFilePath( String resultFilePath )
-        {
-            this.resultFilePath = resultFilePath;
-        }
-
-        public Map<String, String> getDynamicParams()
-        {
-            return dynamicParams;
-        }
-
-        public void setDynamicParams( Map<String, String> dynamicParams )
-        {
-            this.dynamicParams = dynamicParams;
-        }
-    }
-
-    public static ProbeArgs parse( String[] args )
-    {
-        ProbeArgs starterArgs = new ProbeArgs();
-        JCommander jCommander = new JCommander( starterArgs );
-        jCommander.setAcceptUnknownOptions( true );
-        jCommander.parse( args );
-        if ( starterArgs.isHelp() )
-        {
-            jCommander.usage();
-            return null;
-        }
-        return starterArgs;
+        @DynamicParameter(names = "-D", description = "Dynamic parameters go here")
+        private Map<String, String> dynamicParams = new HashMap<>();
     }
 }
